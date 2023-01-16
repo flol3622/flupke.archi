0ab9b0a1b57581f7ca785a0e238d610f4303a68d
philippe <philippe.soubrier@gmail.com>
2023-01-16 15:35:11 +0100

    switch to MUI

--- next.config.js
+++ next.config.js
@@ -4,3 +4,15 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/construction',
+        permanent: true,
+      },
+    ]
+  },
+}
