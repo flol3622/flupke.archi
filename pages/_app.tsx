b547c8d254e9a1b37a6e7bef32f01886bfd009ce
flol3622 <93721496+flol3622@users.noreply.github.com>
2023-01-25 17:51:37 +0100

    canonical

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const canonicalUrl = (`https://flupke.archi/` + (router.asPath === "/" ? "": router.asPath)).split("?")[0];
+
   return (
     <RecoilRoot>
+      <Head>
+        <link rel="canonical" href={canonicalUrl} />
+      </Head>
       <Component {...pageProps} />
     </RecoilRoot>
   );
