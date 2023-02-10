d352b30d833fa42ececbe3eda4f0bd14b013b37a
philippe <philippe.soubrier@gmail.com>
2023-02-10 12:08:16 +0100

    title size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,16 +5,16 @@ import logo from "../public/icons/single-black.svg";
 export default function Navbar() {
   return (
     <nav className="flex">
-      <div className="flex h-16 w-16 items-center justify-center border-b border-black">
-        <Image src={logo} alt="my logo" height={45} />
+      <div className="flex h-14 w-14 items-center justify-center border-b border-black">
+        <Image src={logo} alt="my logo" height={40} />
       </div>
-      <div className="flex h-16 grow items-center justify-center border-b border-l border-black font-title text-2xl">
+      <div className="flex h-14 grow items-center justify-center border-b border-l border-black font-title text-lg sm:text-xl">
         Flupke
       </div>
-      <div className="flex h-16  grow items-center justify-center border-b border-l border-black font-title text-2xl">
+      <div className="text flex h-14 grow items-center justify-center border-b border-l border-black font-title text-lg sm:text-xl">
         Philippe Soubrier
       </div>
-      <div className="flex h-16 w-16 items-center justify-center border-b border-l border-black">
+      <div className="flex h-14 w-14 items-center justify-center border-b border-l border-black">
         <Image src={menuIcon} alt="///" width={24} height={20} />
       </div>
     </nav>
