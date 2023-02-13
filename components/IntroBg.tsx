9e7e256070d52f9a8378dc651190a9bcf9f7d24c
philippe <philippe.soubrier@gmail.com>
2023-02-13 16:17:13 +0100

    blured images

--- components/IntroBg.tsx
+++ components/IntroBg.tsx
@@ -1,12 +1,5 @@
 import Image from "next/image";
-
-function BgImg({ src }: { src: string }) {
-  return (
-    <div className="h-auto w-full">
-      <Image src={src} alt="" width={500} height={500} />
-    </div>
-  );
-}
+import * as Bg from "../public/images/bg";
 
 export default function IntroBg() {
   return (
@@ -16,29 +9,18 @@ export default function IntroBg() {
         className="absolute  top-16 -z-20 w-full max-w-[calc(5*500px)] columns-1 gap-0 mobile:columns-2 md:columns-3 lg:columns-4 xl:columns-5"
         style={{ opacity: "0.15" }}
       >
-        <BgImg src="/images/bg/bg-02.webp" />
-        <BgImg src="/images/bg/bg-03.webp" />
-        <BgImg src="/images/bg/bg-04.webp" />
-        <BgImg src="/images/bg/bg-05.webp" />
-        <BgImg src="/images/bg/bg-06.webp" />
-        <BgImg src="/images/bg/bg-07.webp" />
-        <BgImg src="/images/bg/bg-08.webp" />
-        <BgImg src="/images/bg/bg-09.webp" />
-        <BgImg src="/images/bg/bg-10.webp" />
-        <BgImg src="/images/bg/bg-11.webp" />
-        <BgImg src="/images/bg/bg-12.webp" />
-        <BgImg src="/images/bg/bg-13.webp" />
-        <BgImg src="/images/bg/bg-14.webp" />
-        <BgImg src="/images/bg/bg-15.webp" />
-        <BgImg src="/images/bg/bg-16.webp" />
-        <BgImg src="/images/bg/bg-17.webp" />
-        <BgImg src="/images/bg/bg-18.webp" />
-        <BgImg src="/images/bg/bg-19.webp" />
-        <BgImg src="/images/bg/bg-20.webp" />
-        <BgImg src="/images/bg/bg-21.webp" />
-        <BgImg src="/images/bg/bg-22.webp" />
-        <BgImg src="/images/bg/bg-23.webp" />
-        <BgImg src="/images/bg/bg-24.webp" />
+        {Bg.default.map((item, index) => (
+          <div className="h-auto w-full">
+            <Image
+              placeholder="blur"
+              key={index}
+              src={item}
+              alt=""
+              width={500}
+              height={500}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ public/images/bg/index.tsx
@@ -0,0 +1,49 @@
+import bg02 from "./bg-02.webp";
+import bg03 from "./bg-03.webp";
+import bg04 from "./bg-04.webp";
+import bg05 from "./bg-05.webp";
+import bg06 from "./bg-06.webp";
+import bg07 from "./bg-07.webp";
+import bg08 from "./bg-08.webp";
+import bg09 from "./bg-09.webp";
+import bg10 from "./bg-10.webp";
+import bg11 from "./bg-11.webp";
+import bg12 from "./bg-12.webp";
+import bg13 from "./bg-13.webp";
+import bg14 from "./bg-14.webp";
+import bg15 from "./bg-15.webp";
+import bg16 from "./bg-16.webp";
+import bg17 from "./bg-17.webp";
+import bg18 from "./bg-18.webp";
+import bg19 from "./bg-19.webp";
+import bg20 from "./bg-20.webp";
+import bg21 from "./bg-21.webp";
+import bg22 from "./bg-22.webp";
+import bg23 from "./bg-23.webp";
+import bg24 from "./bg-24.webp";
+
+export default [
+  bg02,
+  bg03,
+  bg04,
+  bg05,
+  bg06,
+  bg07,
+  bg08,
+  bg09,
+  bg10,
+  bg11,
+  bg12,
+  bg13,
+  bg14,
+  bg15,
+  bg16,
+  bg17,
+  bg18,
+  bg19,
+  bg20,
+  bg21,
+  bg22,
+  bg23,
+  bg24,
+];
